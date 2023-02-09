e3eb431648cab0264b11728686e30ec45bb11629
unknown <2723134f@student.gla.ac.uk>
2023-02-09 01:44:59 +0000

    ok

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponce
 
-# Create your views here.
+def index(request):
+    return HttpResponce("Rango says hey there partner!")
+
