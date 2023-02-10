553f5605dd9c5badcd2c428f833a11d3ad87b187
unknown <2723134f@student.gla.ac.uk>
2023-02-10 16:17:31 +0000

    flyimg

--- rango/admin.py
+++ rango/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 from rango.models import Category, Page
-# Register your models here.
-
-admin.site.register(Category)
 
 class PageAdmin():
     list_display = ('title', 'category', 'url')
